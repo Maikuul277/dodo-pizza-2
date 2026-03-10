976d530861f16dd8dc88e8d887516ae86aba0c34
Klimov Stas <stanislav.klimov.2000@mail.ru>
2026-03-10 21:04:39 +0400

    Version 2.2.0 Что поменялось? Доработка JS Кода, оптимизация, доп проверки и bag Фиксы.

--- Source/javascript/ShopJS.js
+++ Source/javascript/ShopJS.js
@@ -1,41 +1,75 @@
+// DOM element
+const cartCount = document.querySelector(".cart-count")
+const buttons = document.querySelectorAll(".add-to-cart")
+const cartIcon = document.querySelector(".cart")
+const cartPopup = document.getElementById("cartPopup")
+const closeCart = document.getElementById("closeCart")
+const cartItems = document.getElementById("cartItems")
+
+
+// Массив карточек корзины
 let cart = []
 
-document.addEventListener("DOMContentLoaded", () => {
 
+// Проверка DOM с начала загружаеться HTML а потом JS
+document.addEventListener("DOMContentLoaded", initCart)
+function initCart(){
+    loadCart()
     updateCartCount()
-
-})
-
-const savedCart = localStorage.getItem("cart")
-
-if (savedCart) {
-    cart = JSON.parse(savedCart)
 }
 
-const buttons = document.querySelectorAll(".add-to-cart")
+
+// Универсальная функция обновления
+function updateCart() {
+    saveCart()
+    renderCart()
+    updateCartCount()
+}
 
 
-    buttons.forEach(button => {
+// Сохранение карточки товара в localStorage
+const savedCart = localStorage.getItem("cart")
 
-        button.addEventListener("click", () => {
+    try {
+        cart = savedCart ? JSON.parse(savedCart) : []
+    } catch {
+        cart = []
+    }
+
+
+// Получение информации о карте товара и добавляем в переменную
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
 
             const product = button.closest(".product")
 
             const name = product.dataset.name
             const price = Number(product.dataset.price)
+            if (isNaN(price) || price < 0) return;
 
             addToCart(name, price)
 
-        })
-
     })
 
+})
 
     saveCart()
     updateCartCount()
 
 
+// защита от спама клика
+buttons.forEach(button => {
+    button.disabled = true
 
+    setTimeout(() => {
+        button.disabled = false
+    },300)
+
+})
+
+
+// Сохранение корзины
 function saveCart() {
 
     localStorage.setItem(
@@ -45,11 +79,11 @@ function saveCart() {
 
 }
 
+
+// Счетчик товаров в корзине
 function updateCartCount() {
 
-    const cartCount = document.querySelector(".cart-count")
-
-    if (!cartCount) return
+    if (!cartCount) return;
 
     cartCount.textContent = cart.reduce(
         (total, item) => total + item.quantity,
@@ -59,61 +93,52 @@ function updateCartCount() {
 
 updateCartCount()
 
-// POPup корзина
-// открытие
 
-const cartIcon = document.querySelector(".cart")
-const cartPopup = document.getElementById("cartPopup")
-
+// POPup корзина и ее открытие
 cartIcon.addEventListener("click", () => {
 
-    cartPopup.style.display = "flex"
-
-    renderCart()
+    if(cartIcon){
+        cartIcon.addEventListener("click", () => {
+            cartPopup.style.display = "flex"
+            renderCart()
+        })
+    }
 
 })
 
+
 //закрытие корзины
-
-const closeCart = document.getElementById("closeCart")
-
 closeCart.addEventListener("click", () => {
 
     cartPopup.style.display = "none"
 
 })
 
-//рендер
 
+//Отображение коорзины
 function renderCart() {
-
-    const cartItems = document.getElementById("cartItems")
 
     cartItems.innerHTML = ""
 
     cart.forEach((item, index) => {
 
         const div = document.createElement("div")
-
         div.className = "cart-item"
 
         div.innerHTML = `
+        <span>${item.name}</span>
 
-<span>${item.name}</span>
+        <div class="cart-quantity">
+        <button class="decrease">-</button>
+        <span>${item.quantity}</span>
+        <button class="increase">+</button>
+        <button class="remove">❌</button>
+        </div>
+        `
 
-<div class="cart-quantity">
-
-<button onclick="decrease(${index})">-</button>
-
-<span>${item.quantity}</span>
-
-<button onclick="increase(${index})">+</button>
-
-<button onclick="removeItem(${index})">❌</button>
-
-</div>
-
-`
+        div.querySelector(".increase").addEventListener("click", () => increase(index))
+        div.querySelector(".decrease").addEventListener("click", () => decrease(index))
+        div.querySelector(".remove").addEventListener("click", () => removeItem(index))
 
         cartItems.appendChild(div)
 
@@ -123,22 +148,18 @@ function renderCart() {
 
 }
 
-// увеличение кол-ва
 
+// увеличение кол-ва
 function increase(index) {
 
     cart[index].quantity++
 
-    saveCart()
-
-    renderCart()
-
-    updateCartCount()
+    updateCart()
 
 }
 
-//уменьшение кол-ва
 
+//уменьшение кол-ва
 function decrease(index) {
 
     cart[index].quantity--
@@ -149,30 +170,22 @@ function decrease(index) {
 
     }
 
-    saveCart()
-
-    renderCart()
-
-    updateCartCount()
+    updateCart()
 
 }
 
-// удаление товара
 
+// удаление товара
 function removeItem(index) {
 
     cart.splice(index, 1)
 
-    saveCart()
-
-    renderCart()
-
-    updateCartCount()
+    updateCart()
 
 }
 
-// общая сумма
 
+// Подсчет общей суммы в корзине
 function updateTotal() {
 
     const total = cart.reduce((sum, item) => {
@@ -185,8 +198,8 @@ function updateTotal() {
 
 }
 
-//рендеркрафт при добавлении товара
 
+//Добавление товара в корзину
 function addToCart(name,price){
 
     const item = cart.find(p=>p.name === name)
@@ -207,10 +220,6 @@ function addToCart(name,price){
 
     }
 
-    saveCart()
-
-    updateCartCount()
-
-    renderCart()
+    updateCart()
 
 }
